11863cbb04b5fb309c97b1ed548710736b11766b
Arkhipov Pavel <p.arkhipov@x5.ru>
2020-01-23 14:46:29 +0300

    delete useless code from App

--- app/src/main/java/me/uptop/prox/App.kt
+++ app/src/main/java/me/uptop/prox/App.kt
@@ -5,27 +5,13 @@ import io.reactivex.plugins.RxJavaPlugins
 import io.sentry.Sentry
 import me.uptop.prox.di.Injector
 import timber.log.Timber
-import java.util.concurrent.TimeUnit
 
 class App : Application() {
-    companion object {
-        private val VIBRATE_INTERVAL_TIME = TimeUnit.SECONDS.toMillis(1L)
-    }
 
     override fun onCreate() {
         super.onCreate()
         Injector.initAppComponent(this)
         Timber.plant(Timber.DebugTree())
-//        maybeSetupNotificationsChannel()
-
-//        if (BuildConfig.DEBUG) {
-//            FirebaseInstanceId
-//                .getInstance()
-//                .instanceId
-//                .addOnSuccessListener { Timber.d(it.token) }
-//
-//            Stetho.initializeWithDefaults(this)
-//        }
 
         if (!BuildConfig.DEBUG) {
             RxJavaPlugins.setErrorHandler {
@@ -33,8 +19,5 @@ class App : Application() {
                 Sentry.capture(it)
             }
         }
-//
-//        initSentry()
-//        initGmsHandlerException()
     }
 }
